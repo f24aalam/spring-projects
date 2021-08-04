c942ae29896cf56d501cf40c8e8c6c2bdb9353b4
Faizan <f24aalam@gmail.com>
2021-08-04 11:18:38 +0530

    Completed the uploading files task

--- uploading-files/src/main/java/com/example/uploadingfiles/FileUploadController.java
+++ uploading-files/src/main/java/com/example/uploadingfiles/FileUploadController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Controller
 public class FileUploadController {
     private final StorageService storageService;
 
